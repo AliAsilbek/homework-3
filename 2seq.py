fe65d5764b32c686c67904dedfbdc18b9761d736
akusa <138038450+AliAsilbek@users.noreply.github.com>
2023-07-15 19:50:16 +0600

    Update 2seq.py

--- 2seq.py
+++ 2seq.py
@@ -1,11 +1,3 @@
-numbers = set()
-print(type(numbers))
 sample = input("Введите элементы списка через запятую: ")
-for i in sample:
-    if not i.isdigit():
-        end = sample.find(i)
-        number = sample[:end]
-        sample = sample.replace(f'{number},', '', 1)
-        numbers.add(number)
-numbers.add(sample)
+numbers = set(sample.split(','))
 print('Результат:', numbers)
