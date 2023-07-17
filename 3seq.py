74565fb593e1c0ba6c3d02243ee7854fdaae9d1a
akusa <138038450+AliAsilbek@users.noreply.github.com>
2023-07-17 11:47:15 +0600

    Update 3seq.py

--- 3seq.py
+++ 3seq.py
@@ -1,12 +1,16 @@
 sample = input("Введите элементы 1-го списка: ")
 numbers = list(sample.split(','))
-
 sample = input("Введите элементы 2-го списка: ")
 second_numbers = list(sample.split(','))
-
-cloned_numbers = numbers
+print(second_numbers)
 for i in numbers:
-    if i in second_numbers:
-        cloned_numbers.remove(i)
-        second_numbers.remove(i)
-print('result:', cloned_numbers)
+    for b in second_numbers:
+        if i == b:
+            numbers[numbers.index(i)] = 'none'
+            second_numbers[second_numbers.index(b)] = 'none1'
+a = []
+for i in numbers:
+    if i.isdigit():
+        a.append(i)
+print('result:', a)
+
